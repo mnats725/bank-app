cc965aa7d59cab25d6862abaaa35028c49ead8b7
Mnats Avetisyan <mnatsteam@mail.ru>
2023-02-01 15:34:47 +0300

    features: test-create-client

---
+++ src/__tests__/createClient.test.ts
@@ -0,0 +1,31 @@
+import { createClient } from "../utils/createClient";
+
+describe("createClient", () => {
+  test("Проверка функции createClient на коректность выполнения", () => {
+    expect(createClient("Mnats", 100)).toEqual({ name: "Mnats", balance: 100 });
+  });
+
+  test("Проверка функции createClient на обработку валидных данных", () => {
+    expect(typeof createClient("Mnats", 7000)).toEqual("object");
+  });
+
+  test("Проверка функции createClient на проверку пустого поля имени", () => {
+    expect(() => {
+      createClient("", 10000);
+    }).toThrow();
+  });
+
+  test("Проверка функции createClient на проверку заглавной буквы имени", () => {
+    expect(() => {
+      createClient("mnats", 10000);
+    }).toThrow();
+  });
+
+  test("Проверка объекта клиента на проверку отсутствия баланса", () => {
+    expect(createClient("Mnats")).toEqual({ name: "Mnats", balance: 0 });
+  });
+
+  test("Проверка функции createClient на отрицательный баланс", () => {
+    expect(createClient("Mnats", -100)).toEqual({ name: "Mnats", balance: -100 });
+  });
+});
--- src/utils/createClient.ts
+++ src/utils/createClient.ts
@@ -1,3 +1,11 @@
-export const createClient = () => {
-  return "createClient";
+export const createClient = (name: string, balance: number = 0) => {
+  let firstNameSymbol = Array.from(name)[0];
+
+  if (name !== "") {
+    if (firstNameSymbol !== firstNameSymbol.toUpperCase()) throw new Error("Имя пишется с заглавной буквы");
+  } else {
+    throw new Error("Введено пустое поле");
+  }
+
+  return { name, balance };
 };
