2b2a039a988d20e7914462e159be3ef57699312e
Mnats Avetisyan <mnatsteam@mail.ru>
2023-02-02 21:38:01 +0300

    refactor: remove some tests

--- src/__tests__/create-client.test.ts
+++ src/__tests__/create-client.test.ts
@@ -14,8 +14,4 @@ describe("createClient", () => {
       createClient("", 10000);
     }).toThrow("Вы ввели пустое поле имени");
   });
-
-  test("Проверка функции createClient на отрицательный баланс", () => {
-    expect(createClient("Mnats", -100)).toEqual({ name: "Mnats", balance: -100 });
-  });
 });
