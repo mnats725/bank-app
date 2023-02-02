1df841edb4c76cf42279304daf673b946cba269f
Mnats Avetisyan <56044398+mnats725@users.noreply.github.com>
2023-02-02 23:15:44 +0300

    test: create client (#1)
    * refactor: file names
    
    * refactor: remove some tests
    
    * refactor: remove some tests
    
    * refactor: remove some tests
    
    * refactor: remove some tests
    
    * refactor: remove some tests
    
    * refactor: remove some tests
    
    * refactor: remove some tests
    
    * refactor: remove some tests
    
    ---------
    
    Co-authored-by: Mnats Avetisyan <mnatsteam@mail.ru>

---
+++ src/__tests__/create-client.test.ts
@@ -0,0 +1,21 @@
+import { createClient } from "../utils/create-client";
+
+describe("createClient", () => {
+  test("Проверка функции createClient на коректность выполнения", () => {
+    expect(createClient("Mnats", 100)).toEqual({ name: "Mnats", balance: 100 });
+  });
+
+  test("Проверка функции createClient на нулевой баланс", () => {
+    expect(createClient("Mnats")).toEqual({ name: "Mnats", balance: 0 });
+  });
+
+  test("Проверка функции createClient на пустое значение поля имени", () => {
+    expect(() => {
+      createClient("", 10000);
+    }).toThrow("Вы ввели пустое поле имени");
+  });
+
+  test("Проверка функции createClient на создание дефолтного значения при отрицательном балансе", () => {
+    expect(createClient("Mnats", -1)).toEqual({ name: "Mnats", balance: 0 });
+  });
+});
---
+++ src/utils/create-client.ts
@@ -0,0 +1,1 @@
+export const createClient = (name: string, balance: number = 0) => ({ name, balance });
