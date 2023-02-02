8319934a8784d60129fdcd63d7c24032b6b29eba
Mnats Avetisyan <mnatsteam@mail.ru>
2023-02-02 23:29:35 +0300

    feat: function createClient

--- src/utils/create-client.ts
+++ src/utils/create-client.ts
@@ -1,1 +1,10 @@
-export const createClient = (name: string, balance: number = 0) => ({ name, balance });
+export const createClient = (name: string, balance: number = 0) => {
+  if (name == "") {
+    throw new Error("Вы ввели пустое поле имени");
+  }
+  if (balance < 0) {
+    balance = 0;
+  }
+  
+  return { name, balance };
+};
