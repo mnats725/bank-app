44b083d2b3d18193aba09f0d5fad400ef29afe55
Mnats Avetisyan <56044398+mnats725@users.noreply.github.com>
2023-02-02 23:50:25 +0300

    feat: createClient
    * feat: function createClient
    
    * refactor: function createClient
    
    * refactor: function createClient
    
    ---------
    
    Co-authored-by: Mnats Avetisyan <mnatsteam@mail.ru>

--- src/utils/create-client.ts
+++ src/utils/create-client.ts
@@ -1,1 +1,11 @@
-export const createClient = (name: string, balance: number = 0) => ({ name, balance });
+export const createClient = (name: string, balance: number = 0) => {
+  if (name === "") {
+    throw new Error("Вы ввели пустое поле имени");
+  }
+
+  if (balance < 0) {
+    return { name, balance: 0 };
+  }
+
+  return { name, balance };
+};
