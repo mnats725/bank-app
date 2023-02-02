8cdc7329b9f37ddb8a2eec4c41007954a1e0315e
Mnats Avetisyan <mnatsteam@mail.ru>
2023-02-02 11:59:46 +0300

    refactor: remove some tests

--- src/__tests__/create-client.test.ts
+++ src/__tests__/create-client.test.ts
@@ -5,22 +5,6 @@ describe("createClient", () => {
     expect(createClient("Mnats", 100)).toEqual({ name: "Mnats", balance: 100 });
   });
 
-  test("Проверка функции createClient на обработку валидных данных", () => {
-    expect(typeof createClient("Mnats", 7000)).toEqual("object");
-  });
-
-  test("Проверка функции createClient на проверку пустого поля имени", () => {
-    expect(() => {
-      createClient("", 10000);
-    }).toThrow();
-  });
-
-  test("Проверка функции createClient на проверку заглавной буквы имени", () => {
-    expect(() => {
-      createClient("mnats", 10000);
-    }).toThrow();
-  });
-
   test("Проверка объекта клиента на проверку отсутствия баланса", () => {
     expect(createClient("Mnats")).toEqual({ name: "Mnats", balance: 0 });
   });
--- src/utils/create-client.ts
+++ src/utils/create-client.ts
@@ -1,11 +1,1 @@
-export const createClient = (name: string, balance: number = 0) => {
-  let firstNameSymbol = Array.from(name)[0];
-
-  if (name !== "") {
-    if (firstNameSymbol !== firstNameSymbol.toUpperCase()) throw new Error("Имя пишется с заглавной буквы");
-  } else {
-    throw new Error("Введено пустое поле");
-  }
-
-  return { name, balance };
-};
+export const createClient = (name: string, balance: number = 0) => ({ name, balance });
