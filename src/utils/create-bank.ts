1a765002de9ec0bcbae0da998d9eb168ef622f2b
Mnats Avetisyan <56044398+mnats725@users.noreply.github.com>
2023-02-03 16:50:29 +0300

    feat: createBank
    Co-authored-by: Mnats Avetisyan <mnatsteam@mail.ru>

--- src/utils/create-bank.ts
+++ src/utils/create-bank.ts
@@ -1,12 +1,18 @@
 import type { Client } from "../types/client";
 
-export const createBank = (bankName: string, clients: Client[] = []) => ({
-  bankName,
-  clients,
-  addClient: function (client: string) {
-    return true;
-  },
-  removeClient: function (client: string) {
-    return true;
-  },
-});
+export const createBank = (bankName: string, clients: Client[] = []) => {
+  if (bankName === "") {
+    throw new Error("Имя банка не может быть пустым!");
+  }
+
+  return {
+    bankName,
+    clients,
+    addClient: function (client: string) {
+      return true;
+    },
+    removeClient: function (client: string) {
+      return true;
+    },
+  };
+};
