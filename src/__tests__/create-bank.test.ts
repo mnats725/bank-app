acc9ba60f54244b4f330244e80a48b2926badf90
Mnats Avetisyan <mnatsteam@mail.ru>
2023-02-03 15:35:44 +0300

    reafactor: test

--- src/__tests__/create-bank.test.ts
+++ src/__tests__/create-bank.test.ts
@@ -15,6 +15,10 @@ describe("createBank", () => {
     expect(() => {
       createBank("");
     }).toThrow("Имя банка не может быть пустым!");
+
+    expect(() => {
+      createBank("", clients);
+    }).toThrow("Имя банка не может быть пустым!");
   });
 
   test("Проверка функции createBank на пустое значение параметра клиента", () => {
