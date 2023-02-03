4b75a351b443ee45da6aef20773da5142424cb3f
Mnats Avetisyan <mnatsteam@mail.ru>
2023-02-03 14:47:12 +0300

    reafactor: test

--- src/__tests__/create-bank.test.ts
+++ src/__tests__/create-bank.test.ts
@@ -2,8 +2,9 @@ import { createBank } from "../utils/create-bank";
 import { createClient } from "../utils/create-client";
 
 describe("createBank", () => {
+  const clients = Array.from({ length: 5 }, (_, index) => createClient(`name ${index}`, 100 + index));
+
   test("Проверка функции createBank на коректность выполнения", () => {
-    const clients = Array.from({ length: 5 }, (_, index) => createClient(`name ${index}`, 100 + index));
     expect(createBank("Сбербанк", clients)).toMatchObject({
       bankName: "Сбербанк",
       clients: clients,
@@ -12,14 +13,14 @@ describe("createBank", () => {
 
   test("Проверка функции createBank на пустое значение поля имени банка", () => {
     expect(() => {
-      createBank("", []);
+      createBank("", clients);
     }).toThrow("Имя банка не может быть пустым!");
   });
 
   test("Проверка функции createBank на пустое значение параметра клиента", () => {
     expect(createBank("Сбербанк")).toMatchObject({
       bankName: "Сбербанк",
-      clients: [],
+      clients: clients,
     });
   });
 });
